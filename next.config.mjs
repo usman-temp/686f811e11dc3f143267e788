e203fc98240e84f324278038a35b915aeb918415
usman-temp <usmanjaved1620@gmail.com>
2025-07-10 14:02:55 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */const nextConfig = {  eslint: {    ignoreDuringBuilds: true,  },};export default nextConfig;
