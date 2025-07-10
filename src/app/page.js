1b68a48c9f30677c5d71051877d4202e0d1b1aa9
usman-temp <usmanjaved1620@gmail.com>
2025-07-10 14:02:27 +0500

    Edit src/app/page.js (lines 0-100)

--- src/app/page.js
+++ src/app/page.js
@@ -4,4 +4,12 @@ export default Home() {
             Hello
         </div>
     )
+import HeroSection from './components/HeroSection';
+export default function Home() {
+  return (
+    <main className="">
+      {' '}
+      <HeroSection />{' '}
+    </main>
+  );
 }
